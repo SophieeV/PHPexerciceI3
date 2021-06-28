b0a03fe43eb320e2ad59937c541c6aacf89bd999
SophieeV <sophievesin@wanadoo.fr>
2021-06-28 14:29:15 +0200

    ajout fichier

---
+++ index.php
@@ -0,0 +1,16 @@
+
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>exercice I3</title>
+  </head>
+  <body>
+  <?php
+
+  echo 'nous sommes le: ' . date('l jS \of F Y');
+  ?>
+  </body>
+</html>
